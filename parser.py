b6c9ce187f0771c335ff89fb437fa3db2c964da3
nzau <abraham.nzau@roamtech.com>
2022-03-01 15:36:20 +0300

    Init

---
+++ parser.py
@@ -0,0 +1,63 @@
+# importing required modules
+import PyPDF2
+import pikepdf
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+# creating a pdf file object
+pdfFilename = 'mpesa_statement.pdf'
+password = "pass"
+pdf = pikepdf.open(pdfFilename, password=password,  allow_overwriting_input=True)
+pdf.save(pdfFilename)
+
+# Create a PdfFileWriter object
+out = PdfFileWriter()
+
+pdfFileObj = open(pdfFilename, 'rb')
+
+# creating a pdf reader object
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+file = PdfFileReader(pdfFilename)
+
+# Check if the opened file is actually Encrypted
+if file.isEncrypted:
+
+    # If encrypted, decrypt it with the password
+    file.decrypt(password)
+
+    # Now, the file has been unlocked.
+    # Iterate through every page of the file
+    # and add it to our new file.
+    for idx in range(file.numPages):
+        # Get the page at index idx
+        page = file.getPage(idx)
+
+        # Add it to the output file
+        out.addPage(page)
+
+    # Open a new file "myfile_decrypted.pdf"
+    with open(pdfFilename, "wb") as f:
+
+        # Write our decrypted PDF to this file
+        out.write(f)
+
+    # Print success message when Done
+    print("File decrypted Successfully.")
+else:
+
+    # If file is not encrypted, print the
+    # message
+    print("File already decrypted.")
+
+# printing number of pages in pdf file
+print(pdfReader.numPages)
+
+# creating a page object
+pageObj = pdfReader.getPage(0)
+
+# extracting text from page
+print(pageObj.extractText())
+
+# closing the pdf file object
+pdfFileObj.close()
